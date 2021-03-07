6f4639ee7349d2032bafa7df04fef1603115c7a2
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-03-07 13:31:42 +0100

    init commit

---
+++ script.ts
@@ -0,0 +1,18 @@
+class Stats { 
+    data1;
+    data2;
+    data3;
+    data4;
+
+    min;
+    max;
+    avg;
+    sum;
+
+    getData() { 
+        this.data1 = document.getElementById("data1");
+        this.data2 = document.getElementById("data2");
+        this.data3 = document.getElementById("data3");
+        this.data4 = document.getElementById("data4");
+    }
+}
