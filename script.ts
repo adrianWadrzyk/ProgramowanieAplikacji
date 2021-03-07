f220697dd67cdaa54b9e18265b4ee4e1767fd967
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-03-07 14:15:49 +0100

    init commit v2

--- script.ts
+++ script.ts
@@ -9,10 +9,19 @@ class Stats {
     avg;
     sum;
 
-    getData() { 
+    getElements() { 
         this.data1 = document.getElementById("data1");
         this.data2 = document.getElementById("data2");
         this.data3 = document.getElementById("data3");
         this.data4 = document.getElementById("data4");
+
+        this.min = document.getElementById("min");
+        this.max = document.getElementById("max");
+        this.avg = document.getElementById("avg");
+        this.sum = document.getElementById("sum");
+    }
+
+    bindEvent() { 
+        this.data1.addEventListener("click", () => console.log("hello") );
     }
 }
