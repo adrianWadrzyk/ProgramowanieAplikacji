9eda280098b6cbd0aaee8367c3b40f61fc3aaf22
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-03-15 17:28:30 +0100

    bug fixed

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-alert("work");
 var Stats = /** @class */ (function () {
     function Stats() {
         this.getElements();
         this.bindEvent();
     }
     Stats.prototype.getElements = function () {
-        this.data1 = document.getElementById("data1");
-        this.data2 = document.getElementById("data2");
-        this.data3 = document.getElementById("data3");
-        this.data4 = document.getElementById("data4");
-        this.min = document.getElementById("min");
-        this.max = document.getElementById("max");
-        this.avg = document.getElementById("avg");
-        this.sum = document.getElementById("sum");
+        this.data1 = document.querySelector("#data1");
+        this.data2 = document.querySelector("#data2");
+        this.data3 = document.querySelector("#data3");
+        this.data4 = document.querySelector("#data4");
+        this.min = document.querySelector("#min");
+        this.max = document.querySelector("#max");
+        this.avg = document.querySelector("#avg");
+        this.sum = document.querySelector("#sum");
     };
     Stats.prototype.bindEvent = function () {
         var _this = this;
-        this.data1.addEventListener("input", function () { return _this.calculateAndShow(); });
-        this.data2.addEventListener("input", function () { return _this.calculateAndShow(); });
-        this.data3.addEventListener("input", function () { return _this.calculateAndShow(); });
-        this.data4.addEventListener("input", function () { return _this.calculateAndShow(); });
+        this.data1.addEventListener("input", function () { return _this.calculateData(); });
+        this.data2.addEventListener("input", function () { return _this.calculateData(); });
+        this.data3.addEventListener("input", function () { return _this.calculateData(); });
+        this.data4.addEventListener("input", function () { return _this.calculateData(); });
     };
-    Stats.prototype.calculateAndShow = function () {
-        var val1 = +this.data1.innerText;
-        var val2 = +this.data2.innerText;
-        var val3 = +this.data3.innerText;
-        var val4 = +this.data4.innerText;
+    Stats.prototype.calculateData = function () {
+        var val1 = +this.data1.value;
+        var val2 = +this.data2.value;
+        var val3 = +this.data3.value;
+        var val4 = +this.data4.value;
         console.log(val1);
-        this.sum = val1 + val2 + val3 + val4;
+        var sum = val1 + val2 + val3 + val4;
+        var avg = sum / 4;
+        var min = Math.min(val1, val2, val3, val4);
+        var max = Math.max(val1, val2, val3, val4);
+        this.showStats(sum, min, max, avg);
+    };
+    Stats.prototype.showStats = function (sum, min, max, avg) {
+        this.max.value = max.toString();
+        this.min.value = min.toString();
+        this.avg.value = avg.toString();
+        this.sum.value = sum.toString();
     };
     return Stats;
 }());
-var stats = new Stats;
+var stats = new Stats();
--- script.ts
+++ script.ts
@@ -1,13 +1,13 @@
 class Stats { 
     data1: HTMLInputElement;
-    data2;
-    data3;
-    data4;
+    data2: HTMLInputElement;
+    data3: HTMLInputElement;
+    data4: HTMLInputElement;
 
-    min;
-    max;
-    avg;
-    sum;
+    min: HTMLInputElement;
+    max: HTMLInputElement;
+    avg: HTMLInputElement;
+    sum: HTMLInputElement;
 
     constructor() { 
         this.getElements();
@@ -15,32 +15,43 @@ class Stats {
     }
 
     getElements() { 
-        this.data1 = document.getElementById("data1").value;
-        this.data2 = document.getElementById("data2");
-        this.data3 = document.getElementById("data3");
-        this.data4 = document.getElementById("data4");
+        this.data1 = document.querySelector("#data1");
+        this.data2 = document.querySelector("#data2");
+        this.data3 = document.querySelector("#data3");
+        this.data4 = document.querySelector("#data4");
 
-        this.min = document.getElementById("min");
-        this.max = document.getElementById("max");
-        this.avg = document.getElementById("avg");
-        this.sum = document.getElementById("sum");
+        this.min = document.querySelector("#min");
+        this.max = document.querySelector("#max");
+        this.avg = document.querySelector("#avg");
+        this.sum = document.querySelector("#sum");
     }
 
     bindEvent() { 
-        this.data1.addEventListener("input", () => this.calculateAndShow() );
-        this.data2.addEventListener("input", () => this.calculateAndShow() );
-        this.data3.addEventListener("input", () => this.calculateAndShow() );
-        this.data4.addEventListener("input", () => this.calculateAndShow() );
+        this.data1.addEventListener("input", () => this.calculateData() );
+        this.data2.addEventListener("input", () => this.calculateData() );
+        this.data3.addEventListener("input", () => this.calculateData() );
+        this.data4.addEventListener("input", () => this.calculateData() );
     }
 
-    calculateAndShow() { 
-        var val1:number = +this.data1.innerText;
-        var val2:number = +this.data2.innerText;
-        var val3:number = +this.data3.innerText;
-        var val4:number = +this.data4.innerText;
-        console.log(val1);
-        this.sum = val1+val2+val3+val4;
+    calculateData() { 
+        var val1:number = +this.data1.value;
+        var val2:number = +this.data2.value;
+        var val3:number = +this.data3.value;
+        var val4:number = +this.data4.value;
+        const sum = val1 + val2 + val3 + val4;
+        const avg = sum/4;
+        const min = Math.min(val1, val2, val3, val4);
+        const max = Math.max(val1, val2, val3, val4);
+
+        this.showStats(sum, min,max, avg);
+    }
+
+    showStats(sum: number, min: number, max: number, avg: number) { 
+        this.max.value = max.toString();
+        this.min.value = min.toString();
+        this.avg.value = avg.toString();
+        this.sum.value = sum.toString();
     }
 }
 
-var stats = new Stats;
+var stats = new Stats();
