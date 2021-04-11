1a2ae72903a4b2da3c3e65dc2323aacb039f728a
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-04-11 14:24:49 +0200

    simple view weather

--- lab3/src/app.ts
+++ lab3/src/app.ts
@@ -6,23 +6,44 @@ export class App {
             const inputElement = (<HTMLInputElement>document.getElementById("city")).value;
             this.getCityInfo(inputElement);
         });
+
+        let storage = this.getData();
+        if(storage.length) 
+            this.getCityInfo(storage);
     }
 
     async getCityInfo(city: string) {
         const weather = await this.getWeather(city);
         this.saveData(weather);
+        this.generateWeatherView(weather);
     }
 
     async getWeather(city: string): Promise<any> {
         const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
         const weatherResponse = await fetch(openWeatherUrl);
         const weatherData = await weatherResponse.json();
-        console.log(weatherData);
         return weatherData;
     }
-    saveData(data: any) {
-        localStorage.setItem('weatherData', JSON.stringify(data));
+
+    generateWeatherView(data : any) { 
+        console.log(data);
+        let div = document.createElement("div");
+        let h3 = document.createElement("h3");
+        let h5 = document.createElement("h5");
+        let spanTemp = document.createElement("span");
+        document.querySelector("body").appendChild(div);
+        h3.textContent = data.name;
+        h5.textContent = data.weather[0].description;
+        spanTemp.textContent = Math.round(data.main.temp - 273).toString();
+        div.appendChild(h3);
+        div.appendChild(h5);
+        div.appendChild(spanTemp);
     }
+
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data.name));
+    }
+
     getData() {
         const data = localStorage.getItem('weatherData');
         if (data) {
