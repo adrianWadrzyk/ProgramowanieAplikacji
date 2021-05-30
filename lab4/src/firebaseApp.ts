d0f2d0ef823fe107de9824ac35bde2207f7691f7
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-05-30 14:30:35 +0200

    basic CRUD

--- lab4/src/firebaseApp.ts
+++ lab4/src/firebaseApp.ts
@@ -1,10 +1,6 @@
 import firebase from 'firebase';
 import {firebaseConfig} from './config';
 import {Interface} from './interface';
-
-
-
-
 class AppFirestorageStorage  implements Interface.IAppStorage{
     private static instance: Interface.IAppStorage;
     db: firebase.firestore.Firestore;
@@ -22,8 +18,25 @@ class AppFirestorageStorage  implements Interface.IAppStorage{
         return AppFirestorageStorage.instance;
     }
 
-    saveData: (data: Interface.INote) => Promise<Interface.INote[]>;
+    async saveData (data : Interface.INote) { 
+        const res = await this.db.collection('notes').add({...data});
+        return Promise.resolve([]);
+    }
+
     removeFromLocalStorage: (id: number) => Promise<Interface.INote[]>;
-    getData: () => Promise<Interface.INote[]>;
-    
+
+    async getData() {
+        const res = await this.db.collection('notes').get().then(res => ({
+            data: res.docs.map((res) => res.data())
+        }));
+        const data = res.data as Interface.INote[];
+        return Promise.resolve(data);
+    };
+
+    async getAllTagsStorage() {
+        const notes = await this.getData();
+        const tags = notes.flatMap((v) => v.tags);
+
+        return Promise.resolve(tags);
+    }
 }
